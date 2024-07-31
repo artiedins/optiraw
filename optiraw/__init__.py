754a0c3cbbe2eefc2600b6b9e8413e1f2c5b8cee
artie <artie@appa.lan>
2024-07-31 12:38:00 -0700

    setup structure

---
+++ optiraw/__init__.py
@@ -0,0 +1,2 @@
+from .functional import fast_dng_to_jpg
+from .functional import hg_dng_to_jpg
---
+++ optiraw/functional.py
@@ -0,0 +1,10 @@
+from .functional import fast_dng_to_jpg
+from .functional import hg_dng_to_jpg
+
+
+def fast_dng_to_jpg(input_dir, output_dir):
+    pass
+
+
+def hq_dng_to_jpg(input_file, output_file, output_size):
+    pass
